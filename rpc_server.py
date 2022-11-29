6814661ba934f869f467b4fa5a2f421f09245f39
romashka <roma.l@mail.ru>
2022-11-29 12:03:48 +0400

    new branch's cleaned

--- rpc_server.py
+++ rpc_server.py
@@ -1,29 +1,13 @@
 import pika
-# from pika.adapters.tornado_connection import TornadoConnection
 
-# connection_tornado = TornadoConnection()
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='rpc_queue')
 
 
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
-
-
 def on_request(ch, method, props, body):
-    # n = int(body)
-    # n = 20
     n = body
-
-    print(" [.] fib(%s)" % n)
-    # response = fib(n)
-    # fib(20)
+    print(" [.] %s" % n)
     response = n
 
     ch.basic_publish(exchange='',
